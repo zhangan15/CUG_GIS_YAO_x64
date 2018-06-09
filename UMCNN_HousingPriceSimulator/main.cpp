79d493d1e36f61011b6e77d8b5d980961495a520
Yao Yao <whuyao@foxmail.com>
2018-06-09 13:22:08 +0800

    add UMCNN_DLIB version

---
+++ UMCNN_HousingPriceSimulator/main.cpp
@@ -0,0 +1,60 @@
+#include <QtCore>
+#include <iostream>
+#include "gdal_priv.h"
+#include <dlib/dnn.h>
+#include <dlib/data_io.h>
+#include "GDALRead.h"
+
+using namespace std;
+using namespace dlib;
+
+//Fitter for simulation
+using net_type = loss_mean_squared<
+	fc<1,
+	dropout<relu<fc<128,
+	dropout<relu<con<32, 3, 3, 1, 1,
+	max_pool<2, 2, 2, 2, relu<con<32, 3, 3, 1, 1,
+	max_pool<2, 2, 2, 2, relu<con<16, 3, 3, 1, 1,
+	input<matrix<rgb_pixel>>
+	>>>>>>>>>>>>>>;
+
+
+struct training_sample
+{
+	matrix<rgb_pixel> input_image;
+	double dHousingPrice; // The ground-truth label of each pixel.
+};
+
+// obtain image block from HSR RS image
+bool obtainImageFromHSR(CGDALRead* pInImg, double dLon, double dLat, int nWidth, int nHeight, matrix<rgb_pixel>& input_image)
+{
+	if (pInImg == NULL)
+		return false;
+
+	//读入数据
+
+
+	//处理
+	input_image.set_size(nWidth, nHeight);
+	
+
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	//load and check GDAL
+	if (GDALGetDriverCount() == 0)
+	{
+		GDALAllRegister();
+		OGRRegisterAll();
+		CPLSetConfigOption("GDAL_FILENAME_IS_UTF8", "NO");	//支持中文路径;
+		CPLSetConfigOption("SHAPE_ENCODING", "");			//支持属性表中的中文字符;
+		cout << "[Init]  environment initialization success!\n";
+	}
+
+
+
+
+	return 0;
+}
